3abbd9fa51340782c406507fbb2edcc7b953f685
Roger <luyaojie1993@gmail.com>
2017-09-19 13:15:31 +0800

    Rename Recurrent

--- pt4nlp/__init__.py
+++ pt4nlp/__init__.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by Roger on 2017/8/26
+from Constants import *
 from convolution import CNNEncoder, MultiSizeCNNEncoder, MultiPoolingCNNEncoder, MultiSizeMultiPoolingCNNEncoder
-from rnn_encoder import RNNEncoder
+from recurrent import RNNEncoder
 from cbow import CBOW
 from dictionary import Dictionary
 from embedding import Embeddings
 
-__all__ = ["CBOW", "CNNEncoder", "Dictionary", "Embeddings", "MultiSizeCNNEncoder", "RNNEncoder",
-           "MultiPoolingCNNEncoder", "MultiSizeMultiPoolingCNNEncoder"]
+__all__ = ["Dictionary",
+           "PAD", "UNK", "BOS", "EOS", "PAD_WORD", "UNK_WORD", "BOS_WORD", "EOS_WORD",
+           "CBOW",
+           "Embeddings",
+           "CNNEncoder", "MultiSizeCNNEncoder", "MultiPoolingCNNEncoder", "MultiSizeMultiPoolingCNNEncoder",
+           "RNNEncoder",]
